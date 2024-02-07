92a852e66b6fa66271b9304186f5e826eb887ab4
bijuyadav <vy395039@gmail.com>
2024-02-08 01:30:18 +0530

    new

--- screens/auth/Login.js
+++ screens/auth/Login.js
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { TextInput } from "react-native-web";
+import BoxInput from "../../Components/Forms/BoxInput";
 
 export default function Login() {
   return (
     <View>
-      <Text>Login</Text>
+      <BoxInput />
     </View>
-  )
+  );
 }
